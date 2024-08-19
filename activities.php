edfb6c00f4ffc381f0cffcb6f2c20fb7e92a51e0
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-08-19 14:06:48 +0630

    🚩🎉 Finished Activities  Page!

---
+++ activities.php
@@ -0,0 +1,47 @@
+<?php
+include_once __DIR__ . '/src/layouts/header.php';
+?>
+
+<!-- banner -->
+<section class="bg-banner h-[350px] bg-center bg-cover relative">
+    <div class="absolute top-[50%] left-[50%] -translate-x-[50%]">
+        <h2 class="banner-text">Our Activities</h2>
+    </div>
+</section>
+
+<section class="h-auto max-w-screen-xl px-6 mx-auto my-24 xl:px-0">
+    <h2 class="mb-4 capitalize heading-h1 animate-fadeIn">Our Activities</h2>
+    <h3 class="mt-10 font-semibold capitalize text-extra-xl animate-fadeIn">How to control of seafarers and our business function</h3>
+    <ul class="mt-6 text-base leading-loose list-disc ms-5 animate-fadeIn">
+        <li>Crew dispatching services to international going vessels under the company’s crew   management contract.</li>
+        <li>Crew selection, replacement arrangements, qualification/certificate management.</li>
+        <li>Instructions, education, and negotiations with local recruiting agents in the country of origin of the crew.</li>
+        <li>Planning and management of crew and on-board joining rotation.</li>
+        <li>Before joining onboard, planning and implementation of Pre-joining Meeting through the nominated Manning Agency.</li>
+        <li>Screening of crew members who excelled well during their services onboard and as recommended by the vessel’s Master for promotion</li>
+        <li>Various customer support related to crew dispatch commissioned by ship owners and ship management companies. (Preparation of contract for salary revision, crew selection proposal, processing of various requests, etc.)</li>
+        <li>Zero customer complaint to regular conduct customer satisfaction survey form ship owner/manager sometimes by email or WeChat</li>
+        <li>To provide 100% safe of seafarers onboard to conduct owner policy guide lines and safety training.</li>
+        <li>To reduce early to repatriation crews due to medical ground.</li>
+        <li>We hire and distribute in the right place according to the needs of the ship owners and complying with the international and national requirement established by Governing Labor Bodies of the individual country of origin of the seafarers employed under the maritime labor Convention of 2006.</li>
+        <li>In addition, we also offer seafarer replacement and recruitment services such as survey in</li>
+        <li>Response to customer requests</li>
+    </ul>
+</section>
+
+<section class="mt-16 bg-[#F4F4F4] pt-[20px] pb-[60px]">
+    <div class="h-auto max-w-screen-xl px-6 mx-auto xl:px-0 animate-fadeIn">
+        <div class="grid gap-8 mt-12 md:grid-cols-2 lg:grid-cols-3">
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+            <div class="h-[450px] bg-[#D9D9D9] rounded-[20px] w-full"></div>
+        </div>
+    </div>
+</section>
+
+<?php
+include_once __DIR__ . '/src/layouts/footer.php';
+?>
