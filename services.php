981d536f19d2791be18ffbdd164cedeab443f619
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-08-19 14:08:14 +0630

    🛠️ Finished Updating Services Page!

--- services.php
+++ services.php
@@ -5,22 +5,22 @@ include_once __DIR__ . '/src/layouts/header.php';
 <!-- banner -->
 <section class="bg-banner h-[350px] bg-center bg-cover relative">
     <div class="absolute top-[50%] left-[50%] -translate-x-[50%]">
-        <h2 class="text-xl font-semibold text-white capitalize md:text-2xl lg:text-3xl xl:text-4xl">Services</h2>
+        <h2 class="banner-text">Services</h2>
     </div>
 </section>
 
 <section class="h-auto max-w-screen-xl px-6 mx-auto my-24 xl:px-0">
-    <h2 class="mb-4 capitalize heading-h1">Our Services</h2>
-    <p class="mt-6 text-base leading-relaxed">
+    <h2 class="mb-4 capitalize heading-h1 animate-fadeIn">Our Services</h2>
+    <p class="mt-6 text-base leading-relaxed animate-fadeIn">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <p class="mt-4 text-base leading-relaxed">
+    <p class="mt-4 text-base leading-relaxed animate-fadeIn">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <p class="mt-4 text-base leading-relaxed">
+    <p class="mt-4 text-base leading-relaxed animate-fadeIn">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <p class="mt-4 text-base leading-relaxed">
+    <p class="mt-4 text-base leading-relaxed animate-fadeIn">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
 </section>
@@ -43,8 +43,8 @@ include_once __DIR__ . '/src/layouts/header.php';
                 <div class="flex justify-center mb-3">
                     <img src="/dist/images/svg/ship.svg" alt="SHIP SVG" class="w-[105px] h-auto">
                 </div>
-                <h3 class="text-center uppercase heading-h3">Ship Management</h3>
-                <p class="mt-5 text-base">
+                <h3 class="text-center uppercase heading-h3 animate-fadeIn">Ship Management</h3>
+                <p class="mt-5 text-base animate-fadeIn">
                     <?= str_limit($text); ?>
                 </p>
             </div>
@@ -52,8 +52,8 @@ include_once __DIR__ . '/src/layouts/header.php';
                 <div class="flex justify-center mb-3">
                     <img src="/dist/images/svg/tech_manage.svg" alt="TECH MANAGE SVG" class="w-[105px] h-auto">
                 </div>
-                <h3 class="text-center uppercase heading-h3">Technical Management</h3>
-                <p class="mt-5 text-base">
+                <h3 class="text-center uppercase heading-h3 animate-fadeIn">Technical Management</h3>
+                <p class="mt-5 text-base animate-fadeIn">
                     <?= str_limit($text); ?>
                 </p>
             </div>
@@ -61,8 +61,8 @@ include_once __DIR__ . '/src/layouts/header.php';
                 <div class="flex justify-center mb-3">
                     <img src="/dist/images/svg/crew.svg" alt="CREW SVG" class="w-[105px] h-auto">
                 </div>
-                <h3 class="text-center uppercase heading-h3">Crew Management</h3>
-                <p class="mt-5 text-base">
+                <h3 class="text-center uppercase heading-h3 animate-fadeIn">Crew Management</h3>
+                <p class="mt-5 text-base animate-fadeIn">
                     <?= str_limit($text); ?>
                 </p>
             </div>
