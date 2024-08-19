4edca5884e99654d67e34917e35a17e4e466f472
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-08-19 14:07:29 +0630

    📫 Finished Contact Us Page!

---
+++ contact-us.php
@@ -0,0 +1,81 @@
+<?php
+include_once __DIR__ . '/src/layouts/header.php';
+?>
+
+<!-- banner -->
+<section class="bg-banner h-[350px] bg-center bg-cover relative">
+    <div class="absolute top-[50%] left-[50%] -translate-x-[50%]">
+        <h2 class="banner-text">Contact Us</h2>
+    </div>
+</section>
+
+<!-- <section class="h-auto max-w-screen-xl px-6 mx-auto my-24 xl:px-0">
+    <div class="grid gap-8 px-20 md:grid-cols-4">
+        <div class="md:col-span-1 animate-fadeIn">
+            <img src="/dist/images/qr.png" alt="QR CODE IMAGE">
+        </div>
+        <div class="md:col-span-3 animate-fadeIn">
+            <h2 class="mb-4 capitalize heading-h2">Contact Us</h2>
+            <div class="flex items-center mt-6">
+                <img src="/dist/images/svg/location.svg" alt="LOCATION SVG" class="me-2">
+                <p class="text-sm md:text-lg ms-2">No.60/62, 8th Floor, 52nd Street, 5 Quarter, Botahtaung Tsp., Yangon Myannmar.</p>
+            </div>
+            <div class="flex items-center mt-4">
+                <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                <a href="tel:+95-95199716" class="text-sm md:text-lg">+95-95199716</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                <a href="tel:+95-785199716" class="text-sm md:text-lg">+95-785199716</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                <a href="mailto:hr.mshipnamagement@gmail.com" class="text-sm md:text-lg">hr.mshipnamagement@gmail.com</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                <a href="mailto:crew@mshipmanagement.com" class="text-sm md:text-lg">crew@mshipmanagement.com</a>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<section class="h-auto max-w-screen-xl px-6 mx-auto my-24 xl:px-0">
+    <div class="grid gap-8 md:px-20 md:grid-cols-4">
+        <div class="flex items-center justify-center md:col-span-1 animate-fadeIn">
+            <img src="/dist/images/qr.png" alt="QR CODE IMAGE" class="h-auto max-w-full">
+        </div>
+        <div class="md:col-span-3 animate-fadeIn">
+            <h2 class="mb-4 text-2xl font-semibold capitalize md:text-3xl">Contact Us</h2>
+            <div class="flex items-start mt-6">
+                <img src="/dist/images/svg/location.svg" alt="LOCATION SVG" class="me-2">
+                <p class="text-sm md:text-lg ms-2">No.60/62, 8th Floor, 52nd Street, 5 Quarter, Botahtaung Tsp., Yangon Myanmar.</p>
+            </div>
+            <div class="flex items-center mt-4">
+                <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                <a href="tel:+95-95199716" class="text-sm md:text-lg">+95-95199716</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                <a href="tel:+95-785199716" class="text-sm md:text-lg">+95-785199716</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                <a href="mailto:hr.mshipnamagement@gmail.com" class="text-sm md:text-lg">hr.mshipnamagement@gmail.com</a>
+            </div>
+            <div class="flex items-center mt-2">
+                <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                <a href="mailto:crew@mshipmanagement.com" class="text-sm md:text-lg">crew@mshipmanagement.com</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="mt-10">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.0693617608545!2d96.16943597492015!3d16.773224384012988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1ec8ae99dfa37%3A0x100a38cd3bff0cea!2sM%20SHIP%20MANAGEMENT%20CO.%2C%20LTD.!5e0!3m2!1sen!2smm!4v1724045807238!5m2!1sen!2smm" width="100%" height="590" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
+
+<?php
+include_once __DIR__ . '/src/layouts/footer.php';
+?>
