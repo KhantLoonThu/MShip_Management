a4e476b109d1a5cd09aee37f672d4d47f423c43b
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-08-19 11:22:33 +0630

    🚨🚩 Finished Updating Footer and Header!

--- src/layouts/footer.php
+++ src/layouts/footer.php
@@ -1,9 +1,9 @@
 <footer class="pt-[50px]">
     <!-- upper footer informations -->
-    <section class="h-auto max-w-screen-xl px-6 pb-10 mx-auto">
-        <div class="grid gap-8 md:grid-cols-3">
-            <div class="flex flex-col">
-                <div class="flex justify-center">
+    <section class="h-auto max-w-screen-xl px-6 mx-auto mb-10 xl:px-0">
+        <div class="grid w-full gap-8 lg:grid-cols-3">
+            <div class="flex flex-col items-center">
+                <div>
                     <img src="/dist/images/logo.png" alt="LOGO Image" class="w-[85px] h-[85px]">
                 </div>
                 <span class="font-semibold text-center uppercase text-extra-xl text-lightGray">M ship management</span>
@@ -14,30 +14,30 @@
                     We have a dedicated support team <br> ready to answer your questions.
                 </p>
             </div>
-            <div class="mx-auto">
-                <div class="flex mb-2">
-                    <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="">
-                    <a href="tel:+95-95199716" class="text-lg">+95-95199716</a>
+            <div class="w-auto mx-auto xl:w-full xl:mx-0">
+                <div class="flex items-center mb-2">
+                    <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                    <a href="tel:+95-95199716" class="text-sm md:text-lg">+95-95199716</a>
                 </div>
-                <div class="flex mb-2">
-                    <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="">
-                    <a href="tel:+95-785199716 " class="text-lg">+95-785199716 </a>
+                <div class="flex items-center mb-2">
+                    <img src="/dist/images/svg/phone.svg" alt="PHONE SVG" class="me-2">
+                    <a href="tel:+95-785199716" class="text-sm md:text-lg">+95-785199716</a>
                 </div>
-                <div class="flex mb-2">
-                    <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="">
-                    <a href="malto:hr.mshipnamagement@gmail.com " class="text-lg">hr.mshipnamagement@gmail.com </a>
+                <div class="flex items-center mb-2">
+                    <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                    <a href="mailto:hr.mshipnamagement@gmail.com" class="text-sm md:text-lg">hr.mshipnamagement@gmail.com</a>
                 </div>
-                <div class="flex mb-2">
-                    <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="">
-                    <a href="malto:crew@mshipmanagement.com" class="text-lg">crew@mshipmanagement.com</a>
+                <div class="flex items-center mb-2">
+                    <img src="/dist/images/svg/mail.svg" alt="MAIL SVG" class="me-2">
+                    <a href="mailto:crew@mshipmanagement.com" class="text-sm md:text-lg">crew@mshipmanagement.com</a>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- copyright section -->
-    <section class="text-center capitalize py-[15px] border-t-2 xl:px-0 border-t-[#F4F4F4]">
-        <span class="text-lightGray">All Rights Reserved 2024 M Ship Management</span>
+    <section class="text-center capitalize py-[15px] px-6 border-t-2 xl:px-0 border-t-[#F4F4F4]">
+        <span class="text-lightGray text-[14px] md:text-base">All Rights Reserved 2024 M Ship Management</span>
     </section>
 </footer>
 
